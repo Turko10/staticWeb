4a27547104a881a7823b8a9bb02cf97224764e12
Kaan Halat <kaanhalat@MacBookPro-KH.local>
2022-10-11 12:24:08 +0200

    update

---
+++ main.js
@@ -0,0 +1,60 @@
+
+//Para declarar un array
+var particiapants = [];
+
+//Para declarar una funcion
+const showParticipants = (data) => {
+    //let para definir una variable
+    let arrayAuxiliar = [];
+
+    data.forEach(element => {
+        arrayAuxiliar.push(element.name);
+    });
+
+    $("#users").text("Participants: " + arrayAuxiliar);
+    $("#users").html("<string>Participants: </string>" + arrayAuxiliar);
+
+}
+
+
+const getWinner = (data) => {
+
+    const aleat = Math.floor(Math.random() * data.length);
+    $("#selected").html("<strong>Seleccionado: </strong>" + data[alert]);
+
+}
+
+
+$(document).ready(function () {
+
+
+    //jsonplaceholder.typicode.com/users
+
+    $.ajax({
+
+        type: "GET",
+        url: "https://jsonplaceholder.typicode.com/users",
+        contentType: "application/json",
+        success: function (response) {
+
+            particiapants = response;
+
+            showParticipants(particiapants);
+
+
+        }, error: function (error) {
+            console.log(error);
+            alert(error);
+        }
+
+    });
+
+    $(".btn").click(function () {
+
+        getWinner(particiapants);
+
+    });
+
+
+
+});
